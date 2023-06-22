a0f9cb2950b2d8b56297e295d21e3305a3dbf458
Abigail <128793760+Abigailnkole96@users.noreply.github.com>
2023-06-22 12:10:30 +0200

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -87,7 +87,7 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
-  
+
     def test_cors_security(self):
         """It should return a CORS header"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
@@ -198,4 +198,3 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-      
